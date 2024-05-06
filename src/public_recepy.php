5b8344b54276114ed2cc5765287ae2cac2db66ed
1u1ucas <lucasleveque17@gmail.com>
2024-05-06 16:58:04 +0200

    Update login and signup pages to improve user experience and fix issues with form inputs and redirects.

--- src/public_recepy.php
+++ src/public_recepy.php
@@ -17,11 +17,6 @@ $recepies = $request->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <section id="post-list" class="contain">
-    <div class="post mb-3 center">
-        <form action="createRecepy.php">
-            <button type="submit" value="envoyer" class="create"> <i class="fa-solid fa-plus"></i></button>
-        </form>
-    </div>
 
     <?php foreach ($recepies as $index => $recepy): ?>
 
@@ -43,6 +38,7 @@ $recepies = $request->fetchAll(PDO::FETCH_ASSOC);
 
                     foreach ($recepy['etape'] as $etape): ?>
                         <li><?php echo htmlspecialchars($etape); ?></li>
+
                     <?php endforeach;
                     ?>
                 </ul>
@@ -50,38 +46,37 @@ $recepies = $request->fetchAll(PDO::FETCH_ASSOC);
             <div class="rightPart">
                 <img class="img" src="<?php echo $recepy['image'] ?>" alt="Image du post">
                 <div class="contain-options">
-                    <form action="recepyUpdate.php" method="POST">
-                        <input type="hidden" name="recepy_id" value="<?php echo $recepy['id']; ?>">
-                        <button type="submit" value="envoyer" class="btn btn-primary update toolTip">
-                            <i class="fa-regular fa-pen-to-square "></i>
-                            <span class="toolTipText">Update</span>
-                        </button>
-                    </form>
-                    <form action="scripts/post/post-delete-script.php" method="POST">
-                        <input type="hidden" name="recepy_id" value="<?php echo $recepy['id']; ?>">
-                        <button type="submit" value="envoyer" class="btn btn-primary update toolTip">
-                            <i class="fa-solid fa-trash-can"></i>
-                            <span class="toolTipText">Delete</span>
-                        </button>
-                    </form>
-                    <form action="scripts/post/post-duplicate-script.php" method="POST">
-                        <input type="hidden" name="recepy_id" value="<?php echo $recepy['id']; ?>">
-                        <button type="submit" value="envoyer" class="btn btn-primary update toolTip">
-                            <i class="fa-solid fa-clone"></i>
-                            <span class="toolTipText">Duplicate</span>
-                        </button>
-                    </form>
-                    <form action="scripts/post/post-copy-script.php" method="POST">
-                        <input type="hidden" name="recepy_id" value="<?php echo $recepy['id']; ?>">
-                        <button type="submit" value="envoyer" class="btn btn-primary update toolTip">
-                            <i class="fa-solid fa-copy"></i>
-                            <span class="toolTipText">Copy</span>
-                        </button>
-                    </form>
+                    <?php
+                    $userId = $_SESSION['id'];
+                    $postId = $recepy['id'];
 
+                    $query = $connectDatabase->prepare("SELECT * FROM allLike WHERE userid = :userid AND recepyid = :recepyid");
+                    $query->bindParam(':userid', $userId);
+                    $query->bindParam(':recepyid', $postId);
+                    $query->execute();
+                    ?>
+                    <?php if ($query->rowCount() > 0): ?>
+                        <form action="scripts/recepy-like/recepy-like.php" method="POST">
+                            <input type="hidden" name="recepy_id" value="<?php echo $recepy['id']; ?>">
+                            <button type="submit" value="envoyer" class="btn btn-primary update toolTip">
+                                <i class="fa-solid fa-heart"></i>
+                                <span class="toolTipText">Unlike</span>
+                            </button>
+                        </form>
+                    <?php else: ?>
+                        <form action="scripts/recepy-like/recepy-like.php" method="POST">
+                            <input type="hidden" name="recepy_id" value="<?php echo $recepy['id']; ?>">
+                            <button type="submit" value="envoyer" class="btn btn-primary update toolTip">
+                                <i class="fa-regular fa-heart"></i>
+                                <span class="toolTipText">Like</span>
+                            </button>
+                        </form>
+                    <?php endif; ?>
+                    <div class="btn btn-primary "> like: <?php echo htmlspecialchars($recepy['liked']) ?>
+                    </div>
                 </div>
-
             </div>
+
         </div>
 
 
